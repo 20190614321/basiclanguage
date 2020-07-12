5259a0ca094bd57b492a7075a8aaa60df0a8b13a
LuckyMan <3208128691@qq.com>
2020-07-12 15:45:16 +0800

    Initial commit

---
+++ src/main/kotlin/main/main.kt
@@ -0,0 +1,15 @@
+package main
+
+fun main(){
+    println("hi jack!")
+    print("hi marry!")
+    println("hi siri!")
+
+    //输出变量
+    val name = "jack"
+    println(name)
+    //输出语句模板
+    println("my name is $name")
+    //当输出的变量值有多个数据时，用${var}
+    println("my name is $name length is ${name.length}")
+}
